d320d9763fb96cafcc84ded36f68dfa438f497be
SmlSBJr <sml.sbjr@gmail.com>
2026-03-11 00:17:49 -0300

    up

---
+++ src/LeetCode.Test/847Test.cs
@@ -0,0 +1,41 @@
+using LeetCode.cs847;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class Test847Test
+    {
+        private static Type[] GetImplementations()
+        {
+            return typeof(Ics847).Assembly.GetTypes()
+                .Where(t => typeof(Ics847).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                .ToArray();
+        }
+
+        public static IEnumerable<object[]> Cases()
+        {
+            yield return new object[] { new int[][] {
+                new[] { 1 },
+                new[] { 0, 2, 4 },
+                new[] { 1, 3, 4 },
+                new[] { 2 },
+                new[] { 1,2 },
+            }, 4 };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Cases), DynamicDataSourceType.Method)]
+        public void ShortestPathLength_Implementations_ReturnExpected(int[][] graph, int expected)
+        {
+            var implementations = GetImplementations();
+            Assert.IsTrue(implementations.Length > 0, "No implementations of Ics847 found.");
+
+            foreach (var implType in implementations)
+            {
+                var solution = (Ics847)Activator.CreateInstance(implType);
+                var result = solution.ShortestPathLength(graph);
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
---
+++ src/LeetCode/847.cs
@@ -0,0 +1,19 @@
+namespace LeetCode.cs847
+{
+    /// <summary>
+    /// https://leetcode.com/problems/shortest-path-visiting-all-nodes/description/
+    /// </summary>
+    public interface Ics847
+    {
+        int ShortestPathLength(int[][] graph);
+    }
+
+    public class cs_1_847 : Ics847
+    {
+        public int ShortestPathLength(int[][] graph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
