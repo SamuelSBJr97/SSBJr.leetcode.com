859932059e51f882ee546b0dfa976284c5e475d9
SmlSBJr <sml.sbjr@gmail.com>
2026-03-29 01:38:48 -0300

    up

---
+++ src/LeetCode.Test/118Test.cs
@@ -0,0 +1,47 @@
+using LeetCode.cs118;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class Test118Test
+    {
+        private static Type[] GetImplementations()
+        {
+            return typeof(Ics118).Assembly.GetTypes()
+                .Where(t => typeof(Ics118).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                .ToArray();
+        }
+
+        public static IEnumerable<object[]> Cases()
+        {
+            yield return new object[] { 5,
+            new int[][]{
+                new[] { 1 },
+                new[] { 1, 1 },
+                new[] { 1,2,1 },
+                new[] { 1,3,3,1 },
+                new[] { 1,4,6,4,1 },
+            } };
+
+            yield return new object[] { 1,
+            new int[][] {
+                new[] { 1 },
+            }};
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Cases), DynamicDataSourceType.Method)]
+        public void Generate_Implementations_ReturnExpected(int nunRows, int[][] row)
+        {
+            var implementations = GetImplementations();
+            Assert.IsTrue(implementations.Length > 0, "No implementations of Ics118 found.");
+
+            foreach (var implType in implementations)
+            {
+                var solution = (Ics118)Activator.CreateInstance(implType);
+                var result = solution.Generate(nunRows);
+                Assert.AreEqual(row, result);
+            }
+        }
+    }
+}
---
+++ src/LeetCode/118.cs
@@ -0,0 +1,18 @@
+namespace LeetCode.cs118
+{
+    /// <summary>
+    /// https://leetcode.com/problems/pascals-triangle
+    /// </summary>
+    public interface Ics118
+    {
+        IList<IList<int>> Generate(int numRows);
+    }
+
+    public class cs_1_118 : Ics118
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+
+        }
+    }
+}
